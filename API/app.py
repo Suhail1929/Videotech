af7a11181f7ff1587d9cbb211441c96dbc6cbea0
suhail1929 <souhailmtarfie@gmail.com>
2023-12-15 10:23:40 +0000

    Volumes

--- API/app.py
+++ API/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, API'
+    return 'Hello, AdddPIs'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- Interface-web/app.py
+++ Interface-web/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Interface web'
+def index():
+    return send_file('./app_code/index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
