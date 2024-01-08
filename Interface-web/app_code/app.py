5f3ed4bccd87bf757050d26e5ba293e8c681d41d
suhail1929 <souhailmtarfie@gmail.com>
2024-01-08 19:28:52 +0000

    Front Modifs

--- Interface-web/app_code/app.py
+++ Interface-web/app_code/app.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, jsonify, render_template, request, flash, redirect, url_for
 from flask_simplelogin import Message, SimpleLogin, login_required
 
-api_url = "http://10.11.5.145:5100"
+api_url = "http://10.11.5.113:5100"
 
 def validate_login(user):
 
